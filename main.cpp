2622f44a0ebdac1c9c4c224fb6aad112b4e730ec
IslandNolan <Nolanboner@icloud.com>
2022-03-28 21:27:18 -0400

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <gtk/gtk.h>
+#include <clasp/config.h>
+#include <clasp/logic_program.h>
+#include <clasp/shared_context.h>
+#include <clasp/unfounded_check.h>
+#include <clasp/model_enumerators.h>
+#include <clasp/solve_algorithms.h>
+
+namespace Clasp{
+        struct Model;
+        class  OutputTable;
+        namespace Asp { class  LogicProgram; }
+}
+
+static void activate (GtkApplication* app,gpointer user_data) {
+    GtkWidget *window;
+    window = gtk_application_window_new(app);
+    gtk_window_set_title(GTK_WINDOW (window), "Project 3");
+    gtk_window_set_default_size(GTK_WINDOW (window), 200, 200);
+    gtk_widget_show_all(window);
+}
+int main(int argc, char **argv) {
+    std::cout << "Hello, World!" << std::endl;
+    GtkApplication *app;
+    int status;
+
+    app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+    g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+    status = g_application_run (G_APPLICATION (app), argc, argv);
+    g_object_unref (app);
+
+    return status;
+
+
+}
