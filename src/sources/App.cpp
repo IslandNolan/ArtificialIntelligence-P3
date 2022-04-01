79547baf5f1b30633a7e39e3584f0050b36c605b
Daniel <DanielHiromoto@outlook.com>
2022-04-01 18:13:57 -0400

    Bind all buttons to a generic test function

--- src/sources/App.cpp
+++ src/sources/App.cpp
@@ -33,6 +33,11 @@ int wininit(int argc, char **argv) {
   auto refBuilder = Gtk::Builder::create();
   FUP files;
   MB but;
+  Gtk::Button *Feas = nullptr;
+  Gtk::Button *Exemp = nullptr;
+  Gtk::Button *Opt = nullptr;
+  Gtk::Button *Omniopt = nullptr;
+
   try {
     // Tell the builder what UI to use
     refBuilder->add_from_file("Project3.glade");
@@ -49,31 +54,79 @@ int wininit(int argc, char **argv) {
   // tell refBuilder what the parent window is
   refBuilder->get_widget("Intelligent Application", pAppWin);
   if (pAppWin) {
+    // m_button1.signal_clicked().connect( sigc::bind<Glib::ustring>(
+    // sigc::mem_fun(*this, &HelloWorld::on_button_clicked), "button 1") );
     // TODO: EVENT HANDLER STUBS HERE
     // Get the GtkBuilder-instantiated Button, and connect a signal handler:
+    // buttons for file upload
     refBuilder->get_widget("Attri_FU_But", files.pAttri);
-    // if getting button was successfull, bind the button to the signal
-    // TODO:  Other buttons only trigger after first button is pressed
-    if (files.pAttri) {
-      files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
     refBuilder->get_widget("Constraint_FU_But", files.pConst);
-    if (files.pConst) {
-      files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
     refBuilder->get_widget("Pen_FU_But", files.pPenL);
-    if (files.pPenL) {
-      files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
     refBuilder->get_widget("Poss_FU_But", files.pPossL);
-    if (files.pPossL) {
-      files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
     refBuilder->get_widget("Qual_FU_But", files.pQual);
-    if (files.pQual) {
+
+    // buttons for Manual Entry Append
+    refBuilder->get_widget("Attri_Man_But", but.pAttri);
+    refBuilder->get_widget("Constraint_Man_But", but.pConst);
+    refBuilder->get_widget("Pen_Man_But", but.pPenL);
+    refBuilder->get_widget("Poss_Man_But", but.pPossL);
+    refBuilder->get_widget("Qual_Man_But", but.pQual);
+
+    // 4 function buttons
+    refBuilder->get_widget("Omni_Opt_But", Omniopt);
+    refBuilder->get_widget("Opt_But", Opt);
+    refBuilder->get_widget("Exemplification_But", Exemp);
+    refBuilder->get_widget("Feasability_But", Feas);
+
+    // if getting button was successfull, bind the button to the signal
+    // Connections to the File Upload Button Entries
+    if (files.pAttri != nullptr) {
       files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
     }
-
+    if (files.pConst != nullptr) {
+      files.pConst->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (files.pPenL != nullptr) {
+      files.pPenL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (files.pPossL != nullptr) {
+      files.pPossL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (files.pQual != nullptr) {
+      files.pQual->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    // Connections to the Manual Button Entries
+    if (but.pAttri != nullptr) {
+      but.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (but.pConst != nullptr) {
+      but.pConst->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (but.pPenL != nullptr) {
+      but.pPenL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (but.pPossL != nullptr) {
+      but.pPossL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (but.pQual != nullptr) {
+      but.pQual->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    // Connections to the 4 functional buttons
+    if (Omniopt != nullptr) {
+      Omniopt->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (Opt != nullptr) {
+      Opt->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (Exemp != nullptr) {
+      Exemp->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (but.pPossL != nullptr) {
+      Feas->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
+    if (but.pQual != nullptr) {
+      but.pQual->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    }
     app->run(*pAppWin);
   }
   return 0;
