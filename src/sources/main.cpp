75e393608edae44af8670da8503f4595e8e56450
xTechon <DanielHiromoto@outlook.com>
2022-04-01 15:32:35 -0400

    Massive Restructure

--- src/sources/main.cpp
+++ src/sources/main.cpp
@@ -1,7 +1,6 @@
-#include "p3/p3.h"
 #include <bits/stdc++.h>
-#include <gtk/gtk.h>
 #include <iostream>
+#include <p3/p3.h>
 #include <string.h>
 
 #define maxAttributes 10
@@ -126,22 +125,8 @@ void logicProcessing(int which) {
   penaltyAndPossibilisticFile.close();
 }
 
-static void activate(GtkApplication *app, gpointer user_data) {
-  GtkWidget *window;
-  window = gtk_application_window_new(app);
-  gtk_window_set_title(GTK_WINDOW(window), "Project 3");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
-  gtk_widget_show_all(window);
-}
 int main(int argc, char **argv) {
   std::cout << "Hello, Gamer!" << std::endl;
-  GtkApplication *app;
-  int status;
-
-  app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
-  g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
-  status = g_application_run(G_APPLICATION(app), argc, argv);
-  g_object_unref(app);
 
   // string task;
   // getline(cin, task);
@@ -284,5 +269,5 @@ int main(int argc, char **argv) {
   // penaltyAndPossibilityStrings, blacklistedBinaries,
   // penaltiesAndProbabilities, penaltiesAndPossibilitiesStack,
   // penaltiesAndPossibilitiesCosts, qualitativeColumn, qualitativeCost);
-  return status;
+  return 0;
 }
