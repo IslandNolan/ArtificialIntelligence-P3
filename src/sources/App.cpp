aa8d48ddc057f4a338438ab5f52f88c57f5ddfa8
xTechon <DanielHiromoto@outlook.com>
2022-04-01 16:55:20 -0400

    Update App.cpp
     - Actually define wininit
     - Create structs to name things easier

--- src/sources/App.cpp
+++ src/sources/App.cpp
@@ -2,14 +2,40 @@
 #include <gtkmm-3.0/gtkmm/applicationwindow.h>
 #include <gtkmm-3.0/gtkmm/builder.h>
 #include <iostream>
+#include <p3/p3.h>
 
 Gtk::ApplicationWindow *pAppWin = nullptr;
 
+class p3::App {
+public:
+  int wininit(int argc, char **argv);
+};
+
+struct FileUp {
+  Gtk::Button *pAttri = nullptr;
+  Gtk::Button *pConst = nullptr;
+  Gtk::Button *pPenL = nullptr;
+  Gtk::Button *pPossL = nullptr;
+  Gtk::Button *pQual = nullptr;
+};
+typedef struct FileUp FUP;
+struct ManBut {
+  Gtk::Button *pAttri = nullptr;
+  Gtk::Button *pConst = nullptr;
+  Gtk::Button *pPenL = nullptr;
+  Gtk::Button *pPossL = nullptr;
+  Gtk::Button *pQual = nullptr;
+};
+
+typedef struct ManBut MB;
+
 static void on_button_clicked() { std::cout << "BUTTON CLICKED" << std::endl; }
 
-int wininit(int argc, char **argv) {
+int p3::App::wininit(int argc, char **argv) {
   auto app = Gtk::Application::create(argc, argv, "org.gtkmm.IntelliApp");
   auto refBuilder = Gtk::Builder::create();
+  FUP files;
+  MB but;
   try {
     refBuilder->add_from_file("Project3.glade");
   } catch (const Glib::FileError &ex) {
@@ -25,11 +51,11 @@ int wininit(int argc, char **argv) {
 
   refBuilder->get_widget("Intelligent Application", pAppWin);
   if (pAppWin) {
+    // TODO: EVENT HANDLER STUBS HERE
     // Get the GtkBuilder-instantiated Button, and connect a signal handler:
-    Gtk::Button *pButton = nullptr;
-    refBuilder->get_widget("Attri_FU_But", pButton);
-    if (pButton) {
-      pButton->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    refBuilder->get_widget("Attri_FU_But", files.pAttri);
+    if (files.pAttri) {
+      files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
     }
 
     app->run(*pAppWin);
