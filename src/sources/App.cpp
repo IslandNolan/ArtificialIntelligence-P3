90ae8e0d99467eec1dc636dfc75f8eb0a71ed982
xTechon <DanielHiromoto@outlook.com>
2022-04-01 17:20:49 -0400

    Added Some documentation and finished namings

--- src/sources/App.cpp
+++ src/sources/App.cpp
@@ -27,11 +27,14 @@ typedef struct ManBut MB;
 static void on_button_clicked() { std::cout << "BUTTON CLICKED" << std::endl; }
 
 int wininit(int argc, char **argv) {
+  // Initalize a new application type
   auto app = Gtk::Application::create(argc, argv, "org.gtkmm.IntelliApp");
+  // Initalize a Builder that will generate the UI later
   auto refBuilder = Gtk::Builder::create();
   FUP files;
   MB but;
   try {
+    // Tell the builder what UI to use
     refBuilder->add_from_file("Project3.glade");
   } catch (const Glib::FileError &ex) {
     std::cerr << "FileError: " << ex.what() << std::endl;
@@ -43,29 +46,30 @@ int wininit(int argc, char **argv) {
     std::cerr << "BuilderError: " << ex.what() << std::endl;
     return 1;
   }
-
+  // tell refBuilder what the parent window is
   refBuilder->get_widget("Intelligent Application", pAppWin);
   if (pAppWin) {
     // TODO: EVENT HANDLER STUBS HERE
     // Get the GtkBuilder-instantiated Button, and connect a signal handler:
-    // TODO: change button string names to correspond to names in .glade file
     refBuilder->get_widget("Attri_FU_But", files.pAttri);
+    // if getting button was successfull, bind the button to the signal
+    // TODO:  Other buttons only trigger after first button is pressed
     if (files.pAttri) {
       files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
     }
-    refBuilder->get_widget("Attri_FU_But", files.pConst);
+    refBuilder->get_widget("Constraint_FU_But", files.pConst);
     if (files.pConst) {
       files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
     }
-    refBuilder->get_widget("Attri_FU_But", files.pPenL);
+    refBuilder->get_widget("Pen_FU_But", files.pPenL);
     if (files.pPenL) {
       files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
     }
-    refBuilder->get_widget("Attri_FU_But", files.pPossL);
+    refBuilder->get_widget("Poss_FU_But", files.pPossL);
     if (files.pPossL) {
       files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
     }
-    refBuilder->get_widget("Attri_FU_But", files.pQual);
+    refBuilder->get_widget("Qual_FU_But", files.pQual);
     if (files.pQual) {
       files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
     }
