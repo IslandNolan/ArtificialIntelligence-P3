04ccb44f2e3664bfe1d14fe4941ea22a3c8d2071
Daniel <DanielHiromoto@outlook.com>
2022-04-01 20:38:07 -0400

    Added many GUI elements
     - TODO: cleanup references list
     - Added textviews and scrollwindows that autoshow scrollbars when the
     textview is too big
     - added many buffers for different categories

--- src/sources/App.cpp
+++ src/sources/App.cpp
@@ -1,12 +1,18 @@
+#include "glibmm/refptr.h"
+#include "gtkmm/enums.h"
 #include <gtkmm-3.0/gtkmm.h>
 #include <gtkmm-3.0/gtkmm/applicationwindow.h>
 #include <gtkmm-3.0/gtkmm/builder.h>
+#include <gtkmm-3.0/gtkmm/scrolledwindow.h>
+#include <gtkmm-3.0/gtkmm/textbuffer.h>
+#include <gtkmm-3.0/gtkmm/textview.h>
 #include <iostream>
 #include <ostream>
 #include <p3/p3.h>
 
 Gtk::ApplicationWindow *pAppWin = nullptr;
 
+// Tidy the different kinds of buttons
 struct FileUp {
   Gtk::Button *pAttri = nullptr;
   Gtk::Button *pConst = nullptr;
@@ -25,20 +31,89 @@ struct ManBut {
 
 typedef struct ManBut MB;
 
+struct FuncBut {
+  Gtk::Button *Feas = nullptr;
+  Gtk::Button *Exemp = nullptr;
+  Gtk::Button *Opt = nullptr;
+  Gtk::Button *Omniopt = nullptr;
+};
+
+typedef struct FuncBut FB;
+
+// Buffers for each kind of data to display to be sent to a text or tree view
+Glib::RefPtr<Gtk::TextBuffer> AttriBuff, ConstBuff, PrefBuff, FeasBuff,
+    ExempBuff, OptBuff, OmniOpBuff;
+// The different view to display the text buffers
+Gtk::TextView *AttriView = nullptr, *ConstView = nullptr, *PrefView = nullptr;
+
+Gtk::ScrolledWindow *AttriScroll = nullptr, *ConstScroll = nullptr,
+                    *PrefScroll = nullptr, *ResScroll = nullptr;
+bool init = false;
+void initBuffers() {
+  std::cout << "INITALIZING BUFFERS" << std::endl;
+  AttriBuff = Gtk::TextBuffer::create();
+  ConstBuff = Gtk::TextBuffer::create();
+  PrefBuff = Gtk::TextBuffer::create();
+  FeasBuff = Gtk::TextBuffer::create();
+  ExempBuff = Gtk::TextBuffer::create();
+  OptBuff = Gtk::TextBuffer::create();
+  OmniOpBuff = Gtk::TextBuffer::create();
+  init = true;
+}
+
+void BufferTestInit() {
+  if (!init) {
+    std::cout << "BUFFERS NOT INITALIZED" << std::endl;
+    return;
+  } else {
+    AttriBuff->set_text("AttriBuff working");
+    ConstBuff->set_text("ConstBuff working");
+    PrefBuff->set_text("PrefBuff working");
+    FeasBuff->set_text("FeasBuff working");
+    ExempBuff->set_text("ExempBuff working");
+    OptBuff->set_text("OptBuff working");
+    OmniOpBuff->set_text("OmniOpBuff working");
+  }
+}
+void initButtons(Gtk::Builder *refBuilder, FUP files, MB but, FB fun) {
+  // Get the GtkBuilder-instantiated Button, and connect a signal handler:
+  // buttons for file upload
+  refBuilder->get_widget("Attri_FU_But", files.pAttri);
+  refBuilder->get_widget("Constraint_FU_But", files.pConst);
+  refBuilder->get_widget("Pen_FU_But", files.pPenL);
+  refBuilder->get_widget("Poss_FU_But", files.pPossL);
+  refBuilder->get_widget("Qual_FU_But", files.pQual);
+
+  // buttons for Manual Entry Append
+  refBuilder->get_widget("Attri_Man_But", but.pAttri);
+  refBuilder->get_widget("Constraint_Man_But", but.pConst);
+  refBuilder->get_widget("Pen_Man_But", but.pPenL);
+  refBuilder->get_widget("Poss_Man_But", but.pPossL);
+  refBuilder->get_widget("Qual_Man_But", but.pQual);
+
+  // 4 function buttons
+  refBuilder->get_widget("Omni_Opt_But", fun.Omniopt);
+  refBuilder->get_widget("Opt_But", fun.Opt);
+  refBuilder->get_widget("Exemplification_But", fun.Exemp);
+  refBuilder->get_widget("Feasability_But", fun.Feas);
+}
+
 static void on_button_clicked() { std::cout << "BUTTON CLICKED" << std::endl; }
 
+// Pass argc and argv from Main
 int wininit(int argc, char **argv) {
+  std::cout << "CREATING APPLICATION" << std::endl;
   // Initalize a new application type
   auto app = Gtk::Application::create(argc, argv, "org.gtkmm.IntelliApp");
   // Initalize a Builder that will generate the UI later
   auto refBuilder = Gtk::Builder::create();
   FUP files;
   MB but;
-  Gtk::Button *Feas = nullptr;
-  Gtk::Button *Exemp = nullptr;
-  Gtk::Button *Opt = nullptr;
-  Gtk::Button *Omniopt = nullptr;
-
+  FB fun;
+  // AttriBuff = Gtk::TextBuffer::create();
+  // AttriBuff->set_text("AttriBuff working");
+  initBuffers();    // initalize all the buffers
+  BufferTestInit(); // fill the buffers with test text
   try {
     // Tell the builder what UI to use
     refBuilder->add_from_file("Project3.glade");
@@ -73,61 +148,51 @@ int wininit(int argc, char **argv) {
     refBuilder->get_widget("Qual_Man_But", but.pQual);
 
     // 4 function buttons
-    refBuilder->get_widget("Omni_Opt_But", Omniopt);
-    refBuilder->get_widget("Opt_But", Opt);
-    refBuilder->get_widget("Exemplification_But", Exemp);
-    refBuilder->get_widget("Feasability_But", Feas);
+    refBuilder->get_widget("Omni_Opt_But", fun.Omniopt);
+    refBuilder->get_widget("Opt_But", fun.Opt);
+    refBuilder->get_widget("Exemplification_But", fun.Exemp);
+    refBuilder->get_widget("Feasability_But", fun.Feas);
 
-    // if getting button was successfull, bind the button to the signal
+    // TODO: get file upload buttons to prompt for file upload
     // Connections to the File Upload Button Entries
-    if (files.pAttri != nullptr) {
-      files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (files.pConst != nullptr) {
-      files.pConst->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (files.pPenL != nullptr) {
-      files.pPenL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (files.pPossL != nullptr) {
-      files.pPossL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (files.pQual != nullptr) {
-      files.pQual->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
+    files.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    files.pConst->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    files.pPenL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    files.pPossL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    files.pQual->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    // TODO: have manual buttons append text to their buffers
     // Connections to the Manual Button Entries
-    if (but.pAttri != nullptr) {
-      but.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (but.pConst != nullptr) {
-      but.pConst->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (but.pPenL != nullptr) {
-      but.pPenL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (but.pPossL != nullptr) {
-      but.pPossL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (but.pQual != nullptr) {
-      but.pQual->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
+    but.pAttri->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    but.pConst->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    but.pPenL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    but.pPossL->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    but.pQual->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
     // Connections to the 4 functional buttons
-    if (Omniopt != nullptr) {
-      Omniopt->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (Opt != nullptr) {
-      Opt->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (Exemp != nullptr) {
-      Exemp->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (but.pPossL != nullptr) {
-      Feas->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    if (but.pQual != nullptr) {
-      but.pQual->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
-    }
-    app->run(*pAppWin);
+    fun.Omniopt->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    fun.Opt->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    fun.Exemp->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+    fun.Feas->signal_clicked().connect(sigc::ptr_fun(on_button_clicked));
+
+    // ScrollViews
+    refBuilder->get_widget("ScrollWinAttriPrev", AttriScroll);
+    refBuilder->get_widget("ScrollWinConstPrev", ConstScroll);
+    refBuilder->get_widget("ScrollWinPrefPrev", PrefScroll);
+    refBuilder->get_widget("ScrollWinResPrev", ResScroll);
+    AttriScroll->set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+    ConstScroll->set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+    PrefScroll->set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+    ResScroll->set_policy(Gtk::POLICY_AUTOMATIC, Gtk::POLICY_AUTOMATIC);
+    // TextViews
+    refBuilder->get_widget("Attri_Prev", AttriView);
+    refBuilder->get_widget("Constraint_Prev", ConstView);
+    refBuilder->get_widget("Pref_Prev", PrefView);
+    AttriView->set_buffer(AttriBuff);
+    ConstView->set_buffer(ConstBuff);
+    PrefView->set_buffer(PrefBuff);
+    // TODO: Add treeview and plug it for results viewing
   }
+
+  // Run the application
+  app->run(*pAppWin);
   return 0;
 }
